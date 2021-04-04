87a596e87454f56052346a76ad67e65b380a60ef
NotaLonelyDay <kalbasa2002@gmail.com>
2021-04-04 13:52:32 +0300

    task_3: refractored entities

--- src/by/epamtc/lonelyday/tasks_01/task3/entity/CircleEntity.java
+++ src/by/epamtc/lonelyday/tasks_01/task3/entity/CircleEntity.java
@@ -1,12 +1,25 @@
 package by.epamtc.lonelyday.tasks_01.task3.entity;
 
 
-public class CircleEntity {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class CircleEntity implements Serializable {
     private double radius;
+
+    public CircleEntity() {
+        radius = 0;
+    }
 
     public CircleEntity(double radius) {
         this.radius = Math.max(radius, 0);
     }
+
+
+    public double calcArea() {
+        return Math.PI * getRadius() * getRadius();
+    }
+
 
     public double getRadius() {
         return radius;
@@ -16,7 +29,24 @@ public class CircleEntity {
         this.radius = Math.max(radius, 0);
     }
 
-    public double calcArea() {
-        return Math.PI * getRadius() * getRadius();
+
+    @Override
+    public String toString() {
+        return "CircleEntity{" +
+                "radius=" + radius +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CircleEntity that = (CircleEntity) o;
+        return Double.compare(that.getRadius(), getRadius()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRadius());
     }
 }
--- src/by/epamtc/lonelyday/tasks_01/task3/entity/SquareEntity.java
+++ src/by/epamtc/lonelyday/tasks_01/task3/entity/SquareEntity.java
@@ -1,12 +1,25 @@
 package by.epamtc.lonelyday.tasks_01.task3.entity;
 
-public class SquareEntity {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class SquareEntity implements Serializable {
 
     private double side;
+
+    public SquareEntity() {
+        this.side = 0;
+    }
 
     public SquareEntity(double side) {
         this.side = Math.max(side, 0);
     }
+
+
+    public double calcArea() {
+        return side * side;
+    }
+
 
     public void setSide(double side) {
         this.side = Math.max(side, 0);
@@ -16,7 +29,25 @@ public class SquareEntity {
         return side;
     }
 
-    public double calcArea() {
-        return side * side;
+
+    @Override
+    public String toString() {
+        return "SquareEntity{" +
+                "side=" + side +
+                '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SquareEntity that = (SquareEntity) o;
+        return Double.compare(that.getSide(), getSide()) == -1;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSide());
+    }
+
 }
