f08923cdd6a5a2837dacc96867598ee41c64b6d2
NotaLonelyDay <kalbasa2002@gmail.com>
2021-04-04 13:48:50 +0300

    task_9: refractored entities

--- src/by/epamtc/lonelyday/tasks_01/task9/entity/CircleEntity.java
+++ src/by/epamtc/lonelyday/tasks_01/task9/entity/CircleEntity.java
@@ -1,10 +1,21 @@
 package by.epamtc.lonelyday.tasks_01.task9.entity;
 
-public class CircleEntity {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class CircleEntity implements Serializable {
     private double r;
 
+
+    public CircleEntity() {
+        r = 0;
+    }
+
     public CircleEntity(double r) {
+        this.r = r;
+    }
+
+    public void setR(double r) {
         this.r = r;
     }
 
@@ -12,12 +23,34 @@ public class CircleEntity {
         return r;
     }
 
-    public double calcArea(){
-        return Math.PI*getR()*getR();
+
+    public double calcArea() {
+        return Math.PI * getR() * getR();
     }
 
-    public double calcLength(){
-        return 2*Math.PI*getR();
+    public double calcLength() {
+        return 2 * Math.PI * getR();
+    }
+
+
+    @Override
+    public String toString() {
+        return "CircleEntity{" +
+                "r=" + r +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CircleEntity that = (CircleEntity) o;
+        return Double.compare(that.getR(), getR()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getR());
     }
 
 }
