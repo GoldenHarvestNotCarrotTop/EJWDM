d25d2835b98fb4f2bebc043d6bc4810266849165
NotaLonelyDay <kalbasa2002@gmail.com>
2021-04-04 13:47:35 +0300

    task_10: refractored entities

--- src/by/epamtc/lonelyday/tasks_01/task10/entity/PointEntity.java
+++ src/by/epamtc/lonelyday/tasks_01/task10/entity/PointEntity.java
@@ -1,19 +1,59 @@
 package by.epamtc.lonelyday.tasks_01.task10.entity;
 
-public class PointEntity {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PointEntity implements Serializable {
+
     public double x;
     public double y;
+
+
+    public PointEntity() {
+        x = 0;
+        y = 0;
+    }
 
     public PointEntity(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
     public double getX() {
         return x;
     }
 
+    public void setY(double y) {
+        this.y = y;
+    }
+
     public double getY() {
         return y;
+    }
+
+    @Override
+    public String toString() {
+        return "PointEntity{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PointEntity that = (PointEntity) o;
+        return Double.compare(that.getX(), getX()) == 0 && Double.compare(that.getY(), getY()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
     }
 }
--- src/by/epamtc/lonelyday/tasks_01/task10/entity/SegmentEntity.java
+++ src/by/epamtc/lonelyday/tasks_01/task10/entity/SegmentEntity.java
@@ -1,13 +1,23 @@
 package by.epamtc.lonelyday.tasks_01.task10.entity;
 
-public class SegmentEntity {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class SegmentEntity implements Serializable {
     private double l;
     private double r;
+
+
+    public SegmentEntity(){
+        l = 0;
+        r = 0;
+    }
 
     public SegmentEntity(double l, double r) {
         this.l = l;
         this.r = r;
     }
+
 
     public double getL() {
         return l;
@@ -15,5 +25,35 @@ public class SegmentEntity {
 
     public double getR() {
         return r;
+    }
+
+    public void setL(double l) {
+        this.l = l;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SegmentEntity that = (SegmentEntity) o;
+        return Double.compare(that.getL(), getL()) == 0 && Double.compare(that.getR(), getR()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getL(), getR());
+    }
+
+    @Override
+    public String toString() {
+        return "SegmentEntity{" +
+                "l=" + l +
+                ", r=" + r +
+                '}';
     }
 }
